0f755a1ca70fc4103723580dcb59ecead5f06551
Tyler Fabian <tylerf808@gmail.com>
2021-08-02 14:13:27 -0400

    Added routes for the home page

---
+++ routes/home.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const path = require("path");
+
+//Home page
+router.get("/", function (req, res) {
+  try {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Exercises
+router.get("/exercises", function (req, res) {
+  try {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Specific exercise
+router.get("/exercise/:id", function (req, res) {
+  try {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Stats page
+router.get("/stats", function (req, res) {
+  try {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
